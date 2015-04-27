02c03d06e8f16d708fb161d2188d21e69fcc185d
winkidney <winkidney@gmail.com>
2015-04-27 10:58:01 -0700

    enhance in json form.

--- pyserver/pypress/json_form.py
+++ pyserver/pypress/json_form.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import json
 import jsonschema
 from jsonschema.validators import Draft4Validator
 
@@ -11,7 +12,7 @@ class BaseField(object):
             <label class="desc" title="{name}" for="{input_id}">{name}<span class="req">*</span> </label>
             <div class="input">
                 <input type="{input_type}" class="form-control {extra_classes}"
-                name="{input_name}" value="0" id="{input_id}">
+                name="{input_name}" value="" id="{input_id}">
             </div>
         </div>
     </div>
@@ -68,6 +69,7 @@ class JsonForm(object):
         if not self.schema:
                 raise NotImplementedError('schema not implemented!')
         if live_schema is not None:
+            self.live_schema = live_schema
             self.schema['properties'].update(live_schema['properties'])
             if "required" in self.schema and "required" in live_schema:
                 self.schema['required'] = list(set(self.schema['required']) | set(live_schema["required"]))
@@ -107,6 +109,14 @@ class JsonForm(object):
                     output[key] = data[key]
 
     @property
+    def dumped_schema(self):
+        return json.dumps(self.schema)
+
+    @staticmethod
+    def loads(input_string):
+        return json.loads(input_string)
+
+    @property
     def fields(self):
         fields = []
         propertries = self.schema['properties']
@@ -121,7 +131,6 @@ class JsonForm(object):
             )
         return fields
 
-
 _schema = {
     "type": "object",
     "properties": {
@@ -130,12 +139,15 @@ _schema = {
             "name": "测试样例",
             "input_id": "to_uid",
             "extra_classes": ["example_class", ],
-            "pattern": "\d+",
+            "pattern": "\d+",  # 正则表达式
         },
     },
     "required": ['to_uid', ],
 }
 
+
+# validation : http://json-schema.org/example2.html
+# 使用额外的字段
 
 def test():
     class Form(JsonForm):
@@ -154,5 +166,45 @@ def test():
     if not form.validate():
         print form.errors
 
+
+# 将所有validation放到form里
+
+def test2():
+    class Form(JsonForm):
+        schema = {
+            "type": "object",
+            "properties": {
+                "test_field": {
+                    "type": "string",    # json_schema 所定义的字段类型
+                    "name": "测试字段2",  # 渲染需要的字段名字
+                    "input_id": "test_field",  # input的ID
+                    "extra_classes": ["example_class", ],  # input的额外类
+                    "pattern": "\d+",  # validation正则表达式
+                },
+            },
+            "required": ['test_field', ],  # 必要字段
+        }
+
+    form_data = {"to_uid": "fuck", "test_field": "haha"}
+
+    form = Form(json_data={}, live_schema=_schema)
+    for field in form.fields:
+        print field.render()
+
+    form = Form(json_data=form_data, live_schema=_schema)
+    if not form.validate():
+        print form.errors
+
+    # 序列化
+    dumped_data = form.dumped_schema
+
+    # 反序列化
+    new_schema = JsonForm.loads(dumped_data)
+
+    # 重新初始化
+    form = Form(json_data=form_data, live_schema=new_schema)
+
+
 if __name__ == "__main__":
     test()
+    test2()
